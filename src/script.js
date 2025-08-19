3fb0a43f94387ba25ffc81a3d645b2b777e69e42
thalsi <thalseehthalsi609@gmail.com>
2025-08-19 23:26:27 +0530

    menu bar

--- src/script.js
+++ src/script.js
@@ -1,5 +1,25 @@
-const conatiner = document.querySelector('.scroll-container');
-conatiner.addEventListener('wheel', (e) => {
+const menu =document.querySelector("[data-menu]");
+const btn =document.querySelector("[data-menu-button]");
+
+btn.addEventListener("click", e=>{
     e.preventDefault();
-    container.scrollLeft += e.deltaY;
+
+
+const spans = btn.querySelectorAll("span");
+  if (menu.classList.contains("active")) {
+
+    menu.classList.remove("active");
+    menu.classList.add("closing");
+    
+    spans[0].classList.add("enabled"); // hide "menu"
+    spans[1].classList.remove("enabled");    // show "close"
+  } else {
+
+    menu.classList.remove("closing");
+    menu.classList.add("active");
+
+    spans[0].classList.remove("enabled");    // show "menu"
+    spans[1].classList.add("enabled"); // hide "close"
+  }
+
 });
